8daf78aa549daf25e9a752d660ad623b757117e2
giannicastillo <66094112+giannicastillo@users.noreply.github.com>
2022-01-19 14:12:34 -0500

    flask fundamentals counter assignment completed!

--- server.py
+++ server.py
@@ -1,15 +1,35 @@
-from flask import Flask, render_template
+from cgitb import reset
+from flask import Flask, render_template, redirect, session, request
 app= Flask (__name__)
+app.secret_key = 'keep it secret, keep it safe'
 
-@app.route('/list')
-def render_list():
-    users = [
-    {'first_name' : 'Michael', 'last_name' : 'Choi'},
-    {'first_name' : 'John', 'last_name' : 'Supsupin'},
-    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-    {'first_name' : 'KB', 'last_name' : 'Tonel'}
-    ]
-    return render_template("list.html", user_info=users)
+@app.route('/')
+def index():
+    if "count" not in session:
+        session["count"]=0
+    return render_template('index.html')
+
+@app.route('/process_count')
+def count_up():
+    session ["count"] +=1
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+
+
+
+
+
+
+
+
+
 
 
 @app.route('/', defaults={'path': ''})
