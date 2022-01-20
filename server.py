0ac105b83fe0712dfcc02054fda0d7148ac6fc25
giannicastillo <66094112+giannicastillo@users.noreply.github.com>
2022-01-19 21:27:44 -0500

    flask fundamentals dojo survey complete

--- server.py
+++ server.py
@@ -1,33 +1,21 @@
 from cgitb import reset
+from os import name
 from flask import Flask, render_template, redirect, session, request
 app= Flask (__name__)
 app.secret_key = 'keep it secret, keep it safe'
 
 @app.route('/')
 def index():
-    if "count" not in session:
-        session["count"]=0
     return render_template('index.html')
 
-@app.route('/process_count')
-def count_up():
-    session ["count"] +=1
-    return redirect('/')
-
-
-@app.route('/reset')
-def reset():
-    session.clear()
-    return redirect('/')
-
-
-
-
-
-
-
-
-
+@app.route('/result',methods=['POST'])
+def nextPage():
+    # name= request.form['name']
+    session['name']=request.form['name']
+    session["Locations"]=request.form['Locations']
+    session['Languages']= request.form['Languages']
+    session['Comments']=request.form['Comments']
+    return render_template('result.html')
 
 
 
